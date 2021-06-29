2c24272817e2689fd23680e7f910bbca5b4150b4
Mueller <muellerwf@nih.gov>
2021-06-29 11:47:19 -0400

    made do_analysis_adv
    do_analysis.sh now is a simple script that requires no user input. It asssumes that the videos to analyze are in /data//to_analyze/ directory

--- utils/do_analysis.sh
+++ utils/do_analysis.sh
@@ -11,31 +11,7 @@ conda activate dlc-windowsGPU
 module load cuDNN/7.0/CUDA-9.0 CUDA/9.0 R/3.5.0 python/3.5
 module load deeplabcut
 
-# determine where videos to analyze are located
-printf "
-\n##############################\n
-	USER INPUT NEEDED
-\n##############################\n
-Are videos stored in \"/data/$USER/to_analyze\"?
-(Y/N):"
-read y_n
-y_n=${y_n,,}
-if [ $y_n == n ]
-then
-printf " 
-\n##############################\n
-	USER INPUT NEEDED
-\n##############################\n
--Enter Video Path (the directory containing 
- the videos needed to be analyzed)
--Enter Video Path using \"/\" to separate directories
--For example \"/data/$USER/testing_folder/to_analyze\"
-\n##############################\n
-Please Enter Video Path:"
-read video_path
-else
 video_path="/data/$USER/to_analyze"
-fi
 printf "This is your video path: $video_path \n" 
 
 ## call sam_video_analysis.py and billy helper.py
